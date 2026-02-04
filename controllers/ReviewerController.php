511a3ad93e7ad3483cc4aefe7a7774518f062ca6
IffatSiddick <saraiffat506@gmail.com>
2026-02-04 14:46:01 +0000

    registration complete

--- controllers/ReviewerController.php
+++ controllers/ReviewerController.php
@@ -1,9 +1,9 @@
 <?php
 class ReviewerController {
-    private DatabaseTable $ReviewerTable;
+    private DatabaseTable $reviewerTable;
 
-    public function __construct(DatabaseTable $reviewersTable) {
-        $this->ReviewerTable = $reviewersTable;
+    public function __construct(DatabaseTable $inputTable) {
+        $this->reviewerTable = $inputTable;
     }
 
     public function registrationform() {
@@ -13,6 +13,50 @@ class ReviewerController {
         ];
     }
 
+        public function regFormSubmit() {
+        $reviewer = $_POST['reviewer'];
+
+        #checking for errors
+        $errors = [];
+
+        if (empty($reviewer['name'])) {
+            $errors[] = "Name cannot be empty.";
+        }
+
+        if (empty($reviewer['email'])) {
+            $errors[] = "Email cannot be empty.";
+        }
+        elseif (filter_var($reviewer['email'], FILTER_VALIDATE_EMAIL) == false) {
+            $errors[] = "Invalid email address";
+        }
+            if (count($this->reviewerTable->find('email', $reviewer['email'])) > 0) {
+                $errors[] = "This email has already been registered";
+            }
+            # check for password length - must be > 12 characters
+            else {
+                $reviewer['email'] = strtolower($reviewer['email']);
+            }
+
+        if (empty($reviewer['password'])) {
+            $errors[] = "Password cannot be empty.";
+        }
+
+        if (empty($errors)) {
+            $reviewer['password'] = password_hash($password, PASSWORD_DEFAULT);;
+            $this->reviewerTable->save($reviewer);
+            header('location: index.php?controller=reviewer&action=success');
+        }
+        else {
+            return [
+            'template' => 'register.html.php',
+            'title' => 'Register an account!',
+            'variables' => [
+                'errors' => $errors,
+                'reviewer' => $reviewer
+                ]
+        ];
+        }
+    }
     public function success() {
         return [
             'template' => 'registerSuccess.html.php',
--- templates/register.html.php
+++ templates/register.html.php
@@ -1,12 +1,23 @@
-<form action="" method="post">
+<?php if (!empty($errors)) : ?>
+    <div class="errors">
+        <p>Your account could not be created. Please check the following:</p>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?=$error ?></li>
+            <?php endforeach; ?>    
+        </ul>
+    </div>
+<?php endif; ?>
+
+<form action="index.php?controller=reviewer&action=regformsubmit" method="post">
     <label for="name">Your name</label>
-    <input name="reviewer[name]" id="name" type="text">
+    <input name="reviewer[name]" id="name" type="text" value="<?=$author['name'] ?? '' ?>">
 
     <label for="email">Your email</label>
-    <input name="reviewer[email]" id="email" type="text">
+    <input name="reviewer[email]" id="email" type="text" value="<?=$author['email'] ?? '' ?>">
 
     <label for="password">Your password</label>
-    <input name="reviewer[pasword]" id="password" type="password">
+    <input name="reviewer[password]" id="password" type="password" value="<?=$author['password'] ?? '' ?>">
 
     <input type="submit" name="submit" value="Register account">
 </form>
